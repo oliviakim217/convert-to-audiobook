b49a9b255918b381443fd17d28227582f3d99841
oliviakim217 <oliviakim217@users.noreply.github.com>
2021-09-18 10:42:05 -0400

    Add requirements.txt

--- application.py
+++ application.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/")
+
+@application.route("/")
 def home():
     return render_template("index.html")
 
 
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    application.debug = True
+    application.run()
